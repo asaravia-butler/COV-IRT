41c6db3c90322bda998f63d5cec8d2218251debc
asaravia-butler <70983120+asaravia-butler@users.noreply.github.com>
2020-11-10 08:37:24 -0800

    Create trimmed_fastqc_unpaired.sh

---
+++ Exact_scripts_used/Mason_cohort1_845samples_exact_scripts/unfiltered_data/01-TrimmedData/trimmed_fastqc_unpaired.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+. ~/.profile
+
+echo "running on $HOSTNAME"
+echo "sample name = $1"
+
+start=$(date +%s)
+echo "start time: $start"
+
+in_dir=/path/to/01-TrimmedData/Fastq
+out_dir=/path/to/01-TrimmedData/FastQC_Reports/unpaired_reads
+
+call="fastqc -t 4 -o $out_dir $in_dir/$1*_U_trimmed.fq.gz"
+
+echo $call
+eval $call
+
+end=$(date +%s)
+echo "end time: $end"
+runtime_s=$(echo $(( end - start )))
+echo "total run time(s): $runtime_s"
+sec_per_min=60
+sec_per_hr=3600
+runtime_m=$(echo "scale=2; $runtime_s / $sec_per_min;" | bc)
+echo "total run time(m): $runtime_m"
+runtime_h=$(echo "scale=2; $runtime_s / $sec_per_hr;" | bc)
+echo "total run time(h): $runtime_h"
