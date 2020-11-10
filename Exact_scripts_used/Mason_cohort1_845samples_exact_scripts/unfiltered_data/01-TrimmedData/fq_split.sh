45a162877cdf78789439aed382c93778b62a3591
asaravia-butler <70983120+asaravia-butler@users.noreply.github.com>
2020-11-10 08:43:03 -0800

    Create fq_split.sh

---
+++ Exact_scripts_used/Mason_cohort1_845samples_exact_scripts/unfiltered_data/01-TrimmedData/fq_split.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+. ~/.profile
+
+echo "running on $HOSTNAME"
+echo "sample name = $1"
+
+start=$(date +%s)
+echo "start time: $start"
+
+in_dir=/path/to/01-TrimmedData/Fastq
+out_dir=/path/to/01-TrimmedData/Fastq_RG
+
+mkdir $out_dir/$1
+
+module load /path/to/COVID19/anaconda3.modulefile
+cd /path/to/COVID19/gdc-fastq-splitter
+source ./path/to/bin/activate
+
+echo "gdc-fastq-splitter version:"
+gdc-fastq-splitter --version
+
+call="gdc-fastq-splitter -o $out_dir/$1/$1_ $in_dir/$1_R1_P_trimmed.fq.gz $in_dir/$1_R2_P_trimmed.fq.gz"
+
+echo $call
+eval $call
+
+end=$(date +%s)
+echo "end time: $end"
+runtime_s=$(echo $(( end - start )))
+echo "total run time(s): $runtime_s"
+sec_per_min=60
+sec_per_hr=3600
+runtime_m=$(echo "scale=2; $runtime_s / $sec_per_min;" | bc)
+echo "total run time(m): $runtime_m"
+runtime_h=$(echo "scale=2; $runtime_s / $sec_per_hr;" | bc)
+echo "total run time(h): $runtime_h"
