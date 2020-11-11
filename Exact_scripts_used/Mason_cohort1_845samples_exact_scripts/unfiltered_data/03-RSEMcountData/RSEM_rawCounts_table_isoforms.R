b31833753528cccd28581936e78c3cb89fb8a42e
asaravia-butler <70983120+asaravia-butler@users.noreply.github.com>
2020-11-11 09:54:31 -0800

    Create RSEM_rawCounts_table_isoforms.R

---
+++ Exact_scripts_used/Mason_cohort1_845samples_exact_scripts/unfiltered_data/03-RSEMcountData/RSEM_rawCounts_table_isoforms.R
@@ -0,0 +1,29 @@
+library(tximport)
+library(tidyverse)
+
+work_dir="/path/to/processing_scripts/03-RSEMcountData"
+counts_dir="/path/to/03-RSEMcountData"
+
+setwd(file.path(work_dir))
+
+### Pull in sample metadata ###
+study <- read.csv(Sys.glob(file.path(work_dir,"samples.txt")), header = FALSE, row.names = 1, stringsAsFactors = TRUE)
+
+
+##### Import Data
+files <- list.files(file.path(counts_dir),pattern = ".isoforms.results", full.names = TRUE)
+# reorder the isoforms.results files to match the ordering of the samples in the samples.txt file
+files <- files[sapply(rownames(study), function(x)grep(x, files, value=FALSE, fixed=TRUE))]
+names(files) <- rownames(study)
+txi.rsem <- tximport(files, type = "rsem", txIn = TRUE, txOut = TRUE)
+
+
+##### Export unnormalized counts table
+setwd(file.path(counts_dir))
+write.csv(txi.rsem$counts,file='RSEM_Unnormalized_Isoform_Counts.csv')
+
+
+## print session info ##
+print("Session Info below: ")
+print("")
+sessionInfo()
