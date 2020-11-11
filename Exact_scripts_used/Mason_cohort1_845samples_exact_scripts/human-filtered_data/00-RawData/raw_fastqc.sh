25a7b2c624ed74a2998721e397f1dda1c26af368
asaravia-butler <70983120+asaravia-butler@users.noreply.github.com>
2020-11-11 15:40:48 -0800

    Create raw_fastqc.sh

---
+++ Exact_scripts_used/Mason_cohort1_845samples_exact_scripts/human-filtered_data/00-RawData/raw_fastqc.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+. ~/.profile
+
+echo "running on $HOSTNAME"
+echo "sample name = $1"
+
+start=$(date +%s)
+echo "start time: $start"
+
+in_dir=/path/to/00-RawData/Fastq
+out_dir=/path/to/00-RawData/FastQC_Reports
+
+call="fastqc -t 4 -o $out_dir $in_dir/$1*fastq.gz"
+
+echo $call
+eval $call
+
+end=$(date +%s)
+echo "end time: $end"
+runtime_s=$(echo $(( end - start )))
+echo "total run time(s): $runtime_s"
+sec_per_min=60
+sec_per_hr=3600
+runtime_m=$(echo "scale=2; $runtime_s / $sec_per_min;" | bc)
+echo "total run time(m): $runtime_m"
+runtime_h=$(echo "scale=2; $runtime_s / $sec_per_hr;" | bc)
+echo "total run time(h): $runtime_h"
