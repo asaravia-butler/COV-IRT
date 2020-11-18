c18ef071b87ab686656688738f32e4714770dcbd
asaravia-butler <70983120+asaravia-butler@users.noreply.github.com>
2020-11-18 12:15:32 -0800

    Create samtools_sort.sh

---
+++ Exact_scripts_used/Mason_cohort1_845samples_exact_scripts/human-filtered_data/02-AlignedData/samtools_sort.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+. ~/.profile
+
+echo "running on $HOSTNAME"
+echo "sample name = $1"
+
+start=$(date +%s)
+echo "start time: $start"
+
+in_dir=/path/to/02-AlignedData_RG
+
+call="samtools sort -m 2500M --threads 4 -o $in_dir/$1/$1_Aligned.sortedByCoord_sorted.out.bam $in_dir/$1/$1_Aligned.sortedByCoord.out.bam"
+
+echo $call
+eval $call
+
+end=$(date +%s)
+echo "end time: $end"
+runtime_s=$(echo $(( end - start )))
+echo "total run time(s): $runtime_s"
+sec_per_min=60
+sec_per_hr=3600
+runtime_m=$(echo "scale=2; $runtime_s / $sec_per_min;" | bc)
+echo "total run time(m): $runtime_m"
+runtime_h=$(echo "scale=2; $runtime_s / $sec_per_hr;" | bc)
+echo "total run time(h): $runtime_h"
