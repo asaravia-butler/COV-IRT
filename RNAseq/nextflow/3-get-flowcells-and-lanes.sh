39b076e64d8d22c887359b5904fc2885a4072a66
Raymond LeClair <raymond.leclair@springbok.io>
2021-02-10 00:26:48 +0000

    Add work in progress

--- RNAseq/nextflow/3-get-flowcells-and-lanes.sh
+++ RNAseq/nextflow/3-get-flowcells-and-lanes.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
+# TODO: Remove
+
 split_reads_dir="../Fastq_Input_Files_for_Testing/split_reads"
 
 sample="ENVHA_332764270_HA_all-reads"
 
-# TODO: Remove basename when in workdir
 flowcells=`ls -1 ${split_reads_dir}/split_${sample}_*_*_R*.fq.gz \
     | xargs -L 1 basename \
     | sed s/split_// \
@@ -49,18 +50,3 @@ for flowcell in ${flowcells}; do
     echo
 
 done
-
-# for flowcell in ${flowcells}; do
-#     echo ${flowcell}
-# 
-#     reads_one_files=`ls -1 ${split_reads_dir}/split_${sample}_${flowcell}_*_R1.fq.gz \
-#         | xargs -L 1 basename \
-#         | tr "\n" ","`
-#     echo ${reads_one_files}
-# 
-#     reads_two_files=`echo ${reads_one_files} | sed s/R1/R2/g`
-#     echo ${reads_two_files}
-# 
-#     echo
-# 
-# done
